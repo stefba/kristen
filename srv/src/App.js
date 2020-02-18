899b10d091742b8e77cfda4057ff40314872410b
stefba <st@rg-s.com>
2020-02-18 16:24:48 +0100

    Replace standard images

--- srv/src/App.js
+++ srv/src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+      <div className="App">
       <header className="App-header">
       <h1>Kristen Mounsey Ceramics</h1>
       </header>
-    </div>
+      </div>
   );
 }
 
