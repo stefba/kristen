1c5df8cc1c4b33681e30eff4ced2e18c03864200
stefba <st@rg-s.com>
2020-04-08 23:48:29 +0200

    Clean up react snap and snapshot

--- site-react/src/index.js
+++ site-react/src/index.js
@@ -6,7 +6,7 @@ import aclient from "./utils/apolloClient";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+ReactDOM.hydrate(
   <React.StrictMode>
     <ApolloProvider client={aclient}>
         <App />
@@ -14,6 +14,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 //
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
