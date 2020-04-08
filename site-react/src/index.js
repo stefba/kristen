8596e2ada39c08d50f50d847452fe18c9fa34cd7
stefba <st@rg-s.com>
2020-04-08 23:45:11 +0200

    Try React server-side rendering

---
+++ site-react/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+//import { render, hydrate } from 'react-dom';
+import ReactDOM from 'react-dom'
+import { ApolloProvider } from "react-apollo";  
+import aclient from "./utils/apolloClient";  
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ApolloProvider client={aclient}>
+        <App />
+    </ApolloProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+//
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
